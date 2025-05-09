c99a0df14b2a0fcb651298834f852d813ac84381
Carlos Avila <cavila@paystand.com>
2025-05-09 13:39:03 -0600

    Adding endpoint to get google token

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, request
+from google.oauth2 import service_account
+import google.auth.transport.requests
+import os
+
+app = Flask(__name__)
+
+# API key should be set as an environment variable
+API_KEY = os.getenv('API_KEY')
+
+def require_api_key(f):
+    def decorated(*args, **kwargs):
+        api_key = request.headers.get('X-API-Key')
+        if api_key and api_key == API_KEY:
+            return f(*args, **kwargs)
+        return jsonify({'error': 'Unauthorized'}), 401
+    decorated.__name__ = f.__name__
+    return decorated
+
+@app.route('/get-google-token', methods=['GET'])
+@require_api_key
+def get_token():
+    SERVICE_ACCOUNT_FILE = 'account_service_credentials.json'
+    SCOPES = ['https://www.googleapis.com/auth/drive',
+              "https://spreadsheets.google.com/feeds"]
+
+    creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES
+    )
+
+    auth_req = google.auth.transport.requests.Request()
+    creds.refresh(auth_req)
+    access_token = creds.token
+
+    return jsonify({'access_token': access_token})
+
+if __name__ == '__main__':
+    app.run(debug=True)
